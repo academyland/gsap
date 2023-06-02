67aae5cbf8b30b2668ef7ad8557ab208277d71bc
acm <academylandnet@gmail.com>
2023-06-02 09:59:17 +0330

    5.overwrite conflicts

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // gsap.to(".img2",{y:100,scale:1.2});
 document.querySelector('#btn').addEventListener('click',()=>{
-    // gsap.from(".img1",{autoAlpha:0,y:-20,duration:4})
-    gsap.fromTo(".img1",{autoAlpha:0,y:-20},{autoAlpha:1,y:0})
+    gsap.from(".img1",{autoAlpha:0,y:-20,duration:4,x:-100})
+    gsap.fromTo(".img1",{autoAlpha:0,y:-20},{autoAlpha:1,y:0,overwrite:'auto'})
 })
