8cd205b2fa12c7b559056222610d703b1989f9e0
acm <academylandnet@gmail.com>
2023-06-02 09:36:34 +0330

    4.fromTo method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // gsap.to(".img2",{y:100,scale:1.2});
 document.querySelector('#btn').addEventListener('click',()=>{
-    gsap.from(".img1",{autoAlpha:0,y:-20,duration:4})
+    // gsap.from(".img1",{autoAlpha:0,y:-20,duration:4})
+    gsap.fromTo(".img1",{autoAlpha:0,y:-20},{autoAlpha:1,y:0})
 })
