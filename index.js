1eab63c60e930c0ebd6f278ee72d24d4e1a9cca1
acm <academylandnet@gmail.com>
2023-06-02 09:01:38 +0330

    3.gsap-from

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-gsap.to(".img2",{y:100,scale:1.2});
+// gsap.to(".img2",{y:100,scale:1.2});
+document.querySelector('#btn').addEventListener('click',()=>{
+    gsap.from(".img1",{autoAlpha:0,y:-20,duration:4})
+})
