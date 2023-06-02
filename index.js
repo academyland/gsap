6461f5ea6f15b79f353044352447b380cf1001fa
acm <academylandnet@gmail.com>
2023-06-02 10:58:29 +0330

    6.ease

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// gsap.to(".img2",{y:100,scale:1.2});
-document.querySelector('#btn').addEventListener('click',()=>{
-    gsap.from(".img1",{autoAlpha:0,y:-20,duration:4,x:-100})
-    gsap.fromTo(".img1",{autoAlpha:0,y:-20},{autoAlpha:1,y:0,overwrite:'auto'})
-})
+gsap.to(".img3",{y:300,scale:1.2,duration:3,ease:'back.out(5)'});
+// document.querySelector('#btn').addEventListener('click',()=>{
+//     gsap.from(".img1",{autoAlpha:0,y:-20,duration:4,x:-100})
+//     gsap.fromTo(".img1",{autoAlpha:0,y:-20},{autoAlpha:1,y:0,overwrite:'auto'})
+// })
