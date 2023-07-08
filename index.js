4c95d7d8bda2aa2213b681e5155a54cd863598ac
acm <academylandnet@gmail.com>
2023-07-08 19:56:21 +0330

    8 events

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 document.querySelector('#btn').addEventListener('click',()=>{
-   gsap.to('.square',{scale:0.1 ,stagger:{
-        amount:0.2,
-        from:'center',
-        grid: 'auto',
-        axis:'y'
-   }
- })
+ gsap.to(".img1",{y:'10rem',repeat:2,
+     onStart:()=>{
+          console.log("onStart")
+     },
+     onComplete:()=>{
+          console.log("onComplete")
+     },
+     onUpdate:()=>{
+          console.log("onUpdate")
+     },
+     onRepeat:()=>{
+          console.log("onRepeat")
+     }
+})
 })
 
