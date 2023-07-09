41018f535bbad415ae3fce722f395ae0ef923e6e
acm <academylandnet@gmail.com>
2023-07-09 19:28:41 +0330

    11 gsap utility methods

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const tween=gsap.to(".img2",{y:100,paused:true,onReverseComplete:()=>{
+     alert("reversed")
+}})
 document.querySelector('#btn').addEventListener('click',()=>{
-     gsap.set(".img1",{y:100,duration:5})
-    
+     tween.duration(5);
+   tween.play();
+   setTimeout(()=>{
+     tween.pause();
+   },1500)
+   setTimeout(()=>{
+     tween.kill();
+     tween.resume().then(()=>{
+          tween.timeScale(10).reverse();
+     });
+   },4000)
 })
 
