6f525451522a268726a850572f777dfae5d2ba3d
acm <academylandnet@gmail.com>
2023-07-21 10:15:31 +0330

    13 define timeline

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-gsap.registerEffect({
-  name:"imageEffect",
-  effect:(targets,config)=>{
-    return gsap.to(targets,{y:200,scale:1.1,rotate:'20deg',duration:config.duration})
-  },
-  defaults:{duration:2}
-})
+const tl=gsap.timeline();
+tl.to(".img1",{y:100,duration:2})
+tl.to(".img2",{y:100,duration:2})
 document.querySelector('#btn').addEventListener('click',()=>{
-  gsap.effects.imageEffect(".img1",{duration:1})
-  gsap.effects.imageEffect(".img2")
+ 
 })
 
