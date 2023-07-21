ad23cd596fb4cab098866c9baf97efce32ac58ef
acm <academylandnet@gmail.com>
2023-07-21 11:54:44 +0330

    16 add label to timeline

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const tl=gsap.timeline({
-  defaults:{
-    duration:1,
-    ease:"back.out(1.7)"
-  },
-  repeat:-1,
-  yoyo:true,
-  paused:true
-});
-tl.to(".img1",{y:100,duration:2})
-.to(".img2",{y:100,duration:2},'<')
-.to(".img3",{y:100,duration:2,ease:'power4.out'},'-=1.5')
-document.querySelector('#btn').addEventListener('click',()=>{
- tl.play();
-})
+  const tl=gsap.timeline({
+    defaults:{
+      duration:1,
+      ease:"back.out(1.7)"
+    },
+    repeat:-1,
+    yoyo:true,
+    paused:true
+  });
+    tl.to(".img1",{y:100,duration:2})
+    .to(".img2",{y:100,duration:2},'<')
+    .addLabel("end")
+    .to(".img3",{y:100,duration:2,ease:'power4.out'},'-=1.5')
+    document.querySelector('#btn').addEventListener('click',()=>{
+      tl.seek('end');
+      tl.play();
+    })
 
