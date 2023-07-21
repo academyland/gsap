70f1c04ccdc31e74e8e07822a3bb118fc5520e57
acm <academylandnet@gmail.com>
2023-07-21 10:44:03 +0330

    14 timeline position params

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tl=gsap.timeline();
 tl.to(".img1",{y:100,duration:2})
-tl.to(".img2",{y:100,duration:2})
+.to(".img2",{y:100,duration:2},'<')
+.to(".img3",{y:100,duration:2},'-=1.5')
 document.querySelector('#btn').addEventListener('click',()=>{
  
 })
