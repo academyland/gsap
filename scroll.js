0b07c0157dec6df7f20c370b8e2cb29d78077218
acm <academylandnet@gmail.com>
2023-08-02 18:43:09 +0330

    20-scrollTrigger pin

--- scroll.js
+++ scroll.js
@@ -1,13 +1,6 @@
-for(i=1;i<=6;i++)
-gsap.from(".img"+i,{
-    autoAlpha:0,
-    // y:-100,
-    scale:0.6,
-    duration:2,
-    scrollTrigger:{
-        trigger:".img"+i,
-        markers:true,
-        start:"top bottom",
-        scrub:2,
-    }
+ScrollTrigger.create({
+    trigger:".img2",
+    pin:true,
+    markers:true,
+    start: "top top+=50%"
 })
