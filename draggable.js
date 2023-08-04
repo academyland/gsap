4655936d14ce01f2033468197906319ab8bcdca1
acm <academylandnet@gmail.com>
2023-08-04 12:22:09 +0330

    23-draggable events

--- draggable.js
+++ draggable.js
@@ -1,5 +1,28 @@
 Draggable.create(".img1",{
     type:"x,y",
     bounds:{width:600,height:600,top:0,left:0},
-    edgeResistance:1
+    edgeResistance:1,
+    lockAxis:true,
+    onClick(){
+        console.log("click")
+    },
+    onPress(){
+        console.log("press")
+    },
+    onDragStart(){
+        console.log("drag start")
+    },
+    onDrag(){
+        console.log("drag")
+    },
+    onDragEnd(){
+        console.log('drag end')
+    },
+    onRelease(){
+        console.log("release")
+    },
+    onLockAxis(){
+        console.log("lock axis")
+    },
+    minimumMovement:30
 })
