2196156dd46f4bb14d27ac979115c0c09b3e5aa1
acm <academylandnet@gmail.com>
2023-08-18 11:51:24 +0330

    23-animated-card project

---
+++ projects/animated-card/index.js
@@ -0,0 +1,14 @@
+tl=gsap.timeline({paused:true});
+tl.to("img",{scale:1.2,duration:3,ease:"power4.out"});
+tl.from(".cart-text",{
+    y:"100%",
+    autoAlpha:0
+},"-=3")
+document.querySelector(".cart").addEventListener("mouseover",()=>{
+    tl.timeScale(1)
+    tl.play();
+})
+document.querySelector(".cart").addEventListener("mouseleave",()=>{
+    tl.timeScale(1.5)
+    tl.reverse();
+})
